2f07cc6b1e5866d3dcaf66dab8b41c783ff40b7f
Bob Park <hwpark@malgn.com>
2025-01-14 16:43:49 +0900

    Initialize Project.

---
+++ src/test/java/com/malgn/ontimeapi/OnTimeApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.malgn.ontimeapi;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTimeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
