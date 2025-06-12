b36a8da54c301fc2857ce7de143add99bd1471cc
Bob Park <hwpark11@kakao.com>
2025-06-12 21:44:45 +0900

    feat: v2 조회 기능 추가

--- src/main/java/com/malgn/ontimeapi/domain/attendance/controller/v2/AttendanceRecordControllerV2.java
+++ src/main/java/com/malgn/ontimeapi/domain/attendance/controller/v2/AttendanceRecordControllerV2.java
@@ -1,16 +1,19 @@
 package com.malgn.ontimeapi.domain.attendance.controller.v2;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.malgn.ontimeapi.domain.attendance.model.AttendanceRecordResponse;
-import com.malgn.ontimeapi.domain.attendance.model.ClockInRequest;
 import com.malgn.ontimeapi.domain.attendance.model.v2.ClockInRequestV2;
 import com.malgn.ontimeapi.domain.attendance.model.v2.ClockOutRequestV2;
+import com.malgn.ontimeapi.domain.attendance.model.v2.GetAttendanceRecordRequestV2;
 import com.malgn.ontimeapi.domain.attendance.service.v2.AttendanceRecordServiceV2;
 
 @RequiredArgsConstructor
@@ -19,6 +22,11 @@ import com.malgn.ontimeapi.domain.attendance.service.v2.AttendanceRecordServiceV
 public class AttendanceRecordControllerV2 {
 
     private final AttendanceRecordServiceV2 recordService;
+
+    @GetMapping(path = "")
+    public List<AttendanceRecordResponse> getRecords(GetAttendanceRecordRequestV2 getRequest) {
+        return recordService.getRecords(getRequest);
+    }
 
     @PostMapping(path = "clock/in")
     public AttendanceRecordResponse clockIn(@RequestBody ClockInRequestV2 clockInRequest) {
